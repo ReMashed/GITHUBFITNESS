f6c7a16f4c4b15383eccad3af6bf16a29a68b25a
ReMashed <ReMashed@users.noreply.github.com>
2018-05-26 00:36:20 +1000

    Profile has stored data , can update and change it

--- Software-Engineering-Studio-1A-Autumn-2018-Wrk2-Group-5-steps-database/app/src/main/java/com/example/group5/fitnessapp/User.java
+++ Software-Engineering-Studio-1A-Autumn-2018-Wrk2-Group-5-steps-database/app/src/main/java/com/example/group5/fitnessapp/User.java
@@ -1,7 +1,70 @@
 package com.example.group5.fitnessapp;
 
 public class User {
+    public String name;
+    public String weight;
+    public String height;
+    public String age;
+    public String intake;
+    public String goal;
 
+    public User(String weight, String height, String age, String intake, String goal) {
+        //this.name = name;
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+        this.intake = intake;
+        this.goal = goal;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public String getIntake() {
+        return intake;
+    }
+
+    public String getGoal() {
+        return goal;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public void setIntake(String intake) {
+        this.intake = intake;
+    }
+
+    public void setGoal(String goal) {
+        this.goal = goal;
+    }
+    /*
     public String email;
     public String password;
     private String Uid;
@@ -26,5 +89,5 @@ public class User {
 
     public String getUid() {
         return this.Uid;
-    }
+    } */
 }
